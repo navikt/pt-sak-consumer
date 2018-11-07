5db8618053356633ea0d3230236270ee647a8220
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-11-07 16:11:19 +0100

    Setter nå config for app under bygging av appen, letter å støtte flere miljøer da

--- src/app.py
+++ src/app.py
@@ -1,34 +1,39 @@
 import logging
-import os
 
 from database import Database
-from kafka import KafkaConsumer
+from dotenv import load_dotenv
+from kafka import KafkaConsumer, TopicPartition
+from os import getenv
 from prometheus_client import Counter, start_http_server
+
+load_dotenv()
 
 
 if __name__ == '__main__':
     start_http_server(8080)
     logging.basicConfig(level=logging.INFO)
-    topic = 'aapen-oppgave-opprettet-v1-preprod'
+    topic = getenv('TOPIC', 'test-producer-topic')
     consumer = KafkaConsumer(
         topic,
-        bootstrap_servers=['b27apvl00045.preprod.local:8443',
-                           'b27apvl00046.preprod.local:8443',
-                           'b27apvl00047.preprod.local:8443'],
-        security_protocol='SASL_SSL',
-        sasl_mechanism='PLAIN',
-        sasl_plain_username=os.environ['SASL_USERNAME'],
-        sasl_plain_password=os.environ['SASL_PASSWORD'],
-        ssl_cafile='/var/run/secrets/nais.io/vault/kafka.pem',
-        group_id='pt-sak-consumer')
+        bootstrap_servers=getenv('BOOTSTRAP_SERVERS',
+                                 'localhost:9092').split(','),
+        security_protocol=getenv('SECURITY_PROTOCOL', 'PLAINTEXT'),
+        sasl_mechanism=getenv('SASL_MECHANISM'),
+        sasl_plain_username=getenv('SASL_USERNAME'),
+        sasl_plain_password=getenv('SASL_PASSWORD'),
+        ssl_cafile=getenv('SSL_CAFILE'),
+        group_id=getenv('GROUP_ID'))
 
-    database = Database(os.environ['DB_URL'])
+    database = Database(
+        getenv('DB_URL',
+               'postgresql+psycopg2://postgres:postgres@localhost:5433/kafka'))
     database.open()
+
     logging.info(f'Consuming messages from {topic}')
     c = Counter('messages', 'Meldinger lest')
 
     for message in consumer:
         logging.info(f'Reading message with offsett={message.offset}')
-        database.store_message(message.value)
+        database.store_message(message.topic, message.value)
         c.inc()
     database.close()
