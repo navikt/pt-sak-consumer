741421927c7a947e175ff1c53db5a3c980534a07
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-02-17 17:11:56 +0100

    Fikset json-logging på traceback Må gjøres med egen exception-handling, blir ikke automatisk gjort

--- src/app.py
+++ src/app.py
@@ -4,35 +4,59 @@ from database import Database
 from kafka import KafkaConsumer
 from os import getenv
 from prometheus_client import Counter, start_http_server
+from pythonjsonlogger import jsonlogger
+
 
 if __name__ == '__main__':
-    start_http_server(8080)
-    logging.basicConfig(
-        format='{"timestamp": "%(asctime)s", "message": "%(name)s:%(message)s", "level": "%(levelname)s"}',
-        level=logging.INFO,
-        datefmt='%Y-%m-%dT%H:%M:%S%z')
-    topics = getenv('TOPICS', 'test-producer-topic').split(',')
-    consumer = KafkaConsumer(
-        *topics,
-        bootstrap_servers=getenv('BOOTSTRAP_SERVERS',
-                                 'localhost:9092').split(','),
-        security_protocol=getenv('SECURITY_PROTOCOL', 'PLAINTEXT'),
-        sasl_mechanism=getenv('SASL_MECHANISM'),
-        sasl_plain_username=getenv('SASL_USERNAME'),
-        sasl_plain_password=getenv('SASL_PASSWORD'),
-        ssl_cafile='/etc/pki/tls/cacert.pem',
-        group_id=getenv('GROUP_ID'))
+    logger = logging.getLogger()
+    logHandler = logging.StreamHandler()
+    format_str = '%(message)%(levelname)%(name)%(asctime)'
+    formatter = jsonlogger.JsonFormatter(format_str)
+    logHandler.setFormatter(formatter)
+    logger.addHandler(logHandler)
 
-    database = Database(
-        getenv('DB_URL',
-               'postgresql+psycopg2://postgres:postgres@localhost:5433/kafka'))
+    start_http_server(8080)
+
+    topics = getenv('TOPICS', 'test-producer-topic').split(',')
+    try:
+        consumer = KafkaConsumer(
+            *topics,
+            bootstrap_servers=getenv('BOOTSTRAP_SERVERS',
+                                     'localhost:9092').split(','),
+            security_protocol=getenv('SECURITY_PROTOCOL', 'PLAINTEXT'),
+            sasl_mechanism=getenv('SASL_MECHANISM'),
+            sasl_plain_username=getenv('SASL_USERNAME'),
+            sasl_plain_password=getenv('SASL_PASSWORD'),
+            ssl_cafile='/etc/pki/tls/cacert.pem',
+            group_id=getenv('GROUP_ID'))
+    except Exception as e:
+        logging.exception(e)
+        logging.shutdown()
+        exit(1)
+
+    db_url = getenv('DB_URL',
+                    'postgresql+psycopg2://postgres:postgres@localhost:5433/kafka')
+    database = Database(db_url)
     database.open()
 
     logging.info(f'Consuming messages from {topics}')
-    c = Counter('messages', 'Meldinger lest')
+    message_read = Counter('messages_read', 'Meldinger lest')
+    storing_failed = Counter('storage_failed',
+                             'Meldinger som har feilet under lagring')
+    message_failed = Counter('messages_failed',
+                             'Meldinger som har feilet under lesing')
 
-    for message in consumer:
-        logging.info(f'Reading message with offsett={message.offset} from partition={message.partition} from topic={message.topic}')
-        database.store_message(message.topic, message.value)
-        c.inc()
-    database.close()
+    try:
+        for message in consumer:
+            logging.info(f'Reading message with offsett={message.offset} from partition={message.partition} from topic={message.topic}')
+            try:
+                database.store_message(message.topic, message.value)
+                message_read.inc()
+            except Exception as e:
+                logging.exception(e)
+                storing_failed.inc()
+            database.close()
+    except Exception as e:
+        logging.exception(e)
+        logging.shutdown()
+        exit(1)
